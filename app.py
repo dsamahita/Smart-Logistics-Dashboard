9b65b6e87eb44e3b73ac7d0ac8b0824b937c9a8e
dsamahita <samahitasart@gmail.com>
2026-04-28 17:08:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+
+# CREATE APP (THIS LINE IS VERY IMPORTANT)
+app = Flask(__name__)
+
+# -------- MODULE 1 --------
+def sort_deliveries():
+    deliveries = [
+        {"id":1, "deadline":5},
+        {"id":2, "deadline":2},
+        {"id":3, "deadline":3}
+    ]
+    deliveries.sort(key=lambda x: x["deadline"])
+    return deliveries
+
+# -------- MODULE 3 --------
+def greedy_knapsack():
+    return 160
+
+# -------- MODULE 4 --------
+def dp_knapsack():
+    return 220
+
+# -------- MODULE 2 --------
+def dijkstra():
+    return {"0→1":3, "0→2":1}
+
+# -------- HOME --------
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# -------- RUN SYSTEM --------
+@app.route("/run")
+def run_system():
+    return render_template(
+        "index.html",
+        deliveries=sort_deliveries(),
+        greedy=greedy_knapsack(),
+        dp=dp_knapsack(),
+        routes=dijkstra()
+    )
+
+# -------- MAIN --------
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
